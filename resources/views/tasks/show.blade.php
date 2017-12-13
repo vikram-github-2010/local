738f1f729e2ee0d6891bf1ff36798913991e833d
vikram <engg.vikram201013@gmail.com>
2017-12-13 12:53:32 +0000

    First commit

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <ul>
+            <li>{{$task->body}}</li>
+          
+        </ul>
+    </body>
+    
+</html>
